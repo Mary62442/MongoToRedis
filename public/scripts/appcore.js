fa019f84567576b601676600015d29333bc5a366
Maria Burlando <maria.bu62442@gmail.com>
2018-01-07 16:32:19 +0100

    Added Angular 1.6

--- public/scripts/appcore.js
+++ public/scripts/appcore.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-	alert('ready');
-})
+	console.log("ready!");
+});
 
+(function() {
+  "use strict";
+  angular.module("api", [])
+  .constant('Title', {
+    value: 'Simple example of Angularjs 1.6'
+  })
+  .controller("MainCtrl", ["Title", MainCtrl]);
+  function MainCtrl(Title) {
+
+  	
+    angular.element(document).ready(function () {
+       alert('from controller');
+    });
+  }
+})();
